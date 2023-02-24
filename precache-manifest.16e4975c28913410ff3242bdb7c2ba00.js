c80c88a56b2b5b6ba7735bbc902de7381fea5a7e
Rahul Jaiswal <rahul.miles5319@gmail.com>
2023-02-24 14:33:48 +0530

    Updates

---
+++ precache-manifest.16e4975c28913410ff3242bdb7c2ba00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03c4d5a07227a0f7bcd8",
+    "url": "/reactwebsite/static/css/main.ef2ac232.chunk.css"
+  },
+  {
+    "revision": "03c4d5a07227a0f7bcd8",
+    "url": "/reactwebsite/static/js/main.03c4d5a0.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/reactwebsite/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/reactwebsite/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "5c1b3b554c555b5d7e22",
+    "url": "/reactwebsite/static/js/runtime~main.5c1b3b55.js"
+  },
+  {
+    "revision": "78caf58f1024fe88deae267574e71411",
+    "url": "/reactwebsite/index.html"
+  }
+];
